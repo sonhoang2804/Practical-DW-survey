6e05340dc22a0361e36ace4c91a4d2082159ad27
sonbig <sonhad00757@fpt.edu.vn>
2020-06-24 10:18:44 +0700

    son

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use App\Category;
+use App\Author;
+use App\Feedback;
+use App\User;
+use Illuminate\Http\Request;
+
+class Webcontroller extends Controller
+{
+    public function surveyForm(){
+        return view("survey");
+    }
+    public function saveFeedback(Request $request){
+        $request->validate([
+            "name"=> "required|string|min:3",
+            "email"=>"required|string",
+            "phone"=>"required",
+            "feedback"=>"required|min:4"
+        ]);
+        try{
+            Feedback::create([
+                "name"=>$request->get("name"),
+                "email"=>$request->get("email"),
+                "phone"=>$request->get("phone"),
+                "feedback"=>$request->get("feedback"),
+            ]);
+
+//            $data["message"] = "thank for your feedback";
+//            notify("global","new_category",$data);
+        }catch (\Exception $exception){
+            return redirect()->back();
+        }
+        return redirect()->to("/");
+    }
+
+}
